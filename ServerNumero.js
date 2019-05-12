22d8897992ee52f62cda2b66a99d690ba3e9e3f5
Esteban Gutierrez <esteban.gutierrez@uptc.edu.co>
2019-05-12 18:50:35 -0500

    Update ServerNumero.js

--- ServerNumero.js
+++ ServerNumero.js
@@ -6,13 +6,6 @@ var app = express();
 
 app.get('/', function (req, res) {
   
-res.send('<b>WikiLeaks publishes a "Highly Confidential" internal document from the cloud computing provider Amazon. The document from late 2015 lists the addresses and some operational details of over one hundred data centers spread across fifteen cities in nine countries. To accompany this document, WikiLeaks also created a map showing where Amazon’s data centers are located.</b>');
-
-});
-
-
-app.get('/getnumber', function (req, res) {
-  
 res.send(Math.floor(Math.random() * 6) + 1);
 
 });
